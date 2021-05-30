f850d8346deca5105edc87c051e870ba1b570176
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2021-05-30 22:33:39 +0700

    Tạo mới một middleware và đăng ký nó vào file Http/Kernel.php (nhóm route admin: routes/web.php)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Enable the Manager middleware
+Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'manager'),
+    function () {
+Route::get('users', 'UsersController@index');
+});
